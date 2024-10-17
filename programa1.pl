e090ca5ec216464b3f871a2d309ff4ff5349053b
ChipanaGerman <gchipanaj@unsa.edu.pe>
2024-10-17 14:55:11 -0500

    Primera version Programa 01

---
+++ programa1.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Pide el nombre de usuario
+print "Ingrese el nombre de usuario: ";
+my $usuario = <STDIN>;
+chomp($usuario);  # Elimina el salto de línea
+
+
