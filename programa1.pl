c57cb8f806e26bc5def6b293c6b8b956cdab99ce
ChipanaGerman <gchipanaj@unsa.edu.pe>
2024-10-17 15:10:20 -0500

    Version final Programa 01

--- programa1.pl
+++ programa1.pl
@@ -7,4 +7,13 @@ print "Ingrese el nombre de usuario: ";
 my $usuario = <STDIN>;
 chomp($usuario);  # Elimina el salto de línea
 
+# Pide el dominio
+print "Ingrese el dominio: ";
+my $dominio = <STDIN>;
+chomp($dominio);  # Elimina el salto de línea
+
+# Imprime el correo utilizando interpolación
+print "$usuario\@$dominio\n";
+
+
 
